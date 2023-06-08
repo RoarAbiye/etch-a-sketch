c108c42911e9fd6c18508de85567934862669189
robelabiye <rblabiye5@gmail.com>
2023-06-08 23:27:17 +0300

    Finished the project with minimum requirements

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const gridContainer = document.querySelector('#gridContainer');
 const setSizeButton = document.querySelector('.setSizeButton');
 const clearButton = document.querySelector('.clearButton');
+const drawingModeElement = document.getElementsByName('drawingMode');
 
 const setGridSize = () => {
   const customeGridSize = prompt();
   gridContainer.innerHTML = '';
-  gridContainer.appendChild(gridGenerator(customeGridSize))
-  refreshEventListner()
+  gridContainer.appendChild(gridGenerator(customeGridSize));
+  refreshEventListner('regular');
 };
 
 setSizeButton.onclick = () => setGridSize();
@@ -14,7 +15,7 @@ setSizeButton.onclick = () => setGridSize();
 const DEFAULT_SIZE = 64;
 
 function gridGenerator(gridSize) {
-  const newGridElements = document.createElement('div')
+  const newGridElements = document.createElement('div');
 
   for (let i = 0; i < gridSize; i++) {
     let row = document.createElement('div');
@@ -34,25 +35,42 @@ function gridGenerator(gridSize) {
   return newGridElements;
 }
 
-
-console.log(gridGenerator(DEFAULT_SIZE));
 gridContainer.appendChild(gridGenerator(DEFAULT_SIZE));
-refreshEventListner()
+refreshEventListner('regular');
 
-
-
-clearButton.textContent = 'clear';
 clearButton.onclick = () => {
   gridContainer.innerHTML = '';
   gridContainer.appendChild(gridGenerator(DEFAULT_SIZE));
-  refreshEventListner()
+  refreshEventListner('regular');
 };
 
-function refreshEventListner () {
-const cells = document.querySelectorAll('.gridsquare')
-  cells.forEach(function(cell) {
-    cell.addEventListener('mouseover', (event) => {
-      event.target.style.backgroundColor = 'black'
+drawingModeElement.forEach((mode) => {
+  mode.onclick = () => {
+    refreshEventListner(mode.value);
+    console.log('drawing mode changed to ', mode.value);
+  };
+});
+
+function refreshEventListner(drawingMode) {
+  if (drawingMode == 'regular') {
+    const cells = document.querySelectorAll('.gridsquare');
+    cells.forEach(function (cell) {
+      cell.addEventListener('mouseover', (event) => {
+        event.target.style.backgroundColor = 'black';
+      });
     });
-  })
+    console.log('drwing with ', drawingMode);
+  } else if (drawingMode == 'random') {
+    const cells = document.querySelectorAll('.gridsquare');
+    cells.forEach(function (cell) {
+      cell.addEventListener('mouseover', (event) => {
+        event.target.style.backgroundColor = `rgb( ${Math.floor(
+          Math.random() * 254 + 1
+        )} ${Math.floor(Math.random() * 254 + 1)}, ${Math.floor(
+          Math.random() * 254 + 1
+        )} )`;
+      });
+    });
+    console.log('drwing with ', drawingMode);
+  }
 }
